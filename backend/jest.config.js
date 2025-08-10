e444ee3fbe411c912a8d831a29ba7fca2692f8ef
kubaparol <kubciop33@gmail.com>
2025-08-10 13:35:13 +0200

    chore: update Jest configuration to ignore node_modules and dist directories

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'], // Only test files inside `tests/` folder
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  modulePathIgnorePatterns: ['/dist/'],
 };
